39c7c4ff35a405c68197de5c96c8158e96facab4
Darshan4518 <codewithdarshan45@gmail.com>
2025-07-07 20:18:04 +0530

    feat(about):add brand slider

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,7 @@ import CompanyValues from "@/components/about/company-values";
 import FaqSection from "@/components/about/faq-section";
 import TeamSection from "@/components/about/team-section";
 import WhoWeAre from "@/components/about/who-we-are";
+import FooterBrandSlider from "@/components/FooterBrandSlider";
 import FooterTextSlider from "@/components/FooterTextSlider";
 import TestimonialsSection from "@/components/home/testimonials-section";
 
@@ -9,7 +10,7 @@ const About = () => {
   return (
     <div>
       {/* Hero banner image at the top of the About page */}
-      <div className="min-h-[50vh] h-[50vh]">
+      <div className="min-h-[50vh] h-[50vh] ">
         <img
           src="./About.jpg"
           alt="About page banner"
@@ -25,6 +26,18 @@ const About = () => {
 
       {/* Section introducing the team members */}
       <TeamSection />
+
+      {/* Section introducing the Brands */}
+      <div className=" max-w-[90%] mx-auto pb-20">
+        <div className="flex flex-col sm:flex-row justify-center items-center my-10 gap-4 text-center">
+          <div className="w-full sm:w-[40%] bg-gray-500/40 h-0.5" />
+          <h2 className="text-base sm:text-lg font-medium">
+            Global Trusted Partner
+          </h2>
+          <div className="w-full sm:w-[40%] bg-gray-500/40 h-0.5" />
+        </div>
+        <FooterBrandSlider />
+      </div>
 
       {/* Frequently Asked Questions section */}
       <FaqSection />
